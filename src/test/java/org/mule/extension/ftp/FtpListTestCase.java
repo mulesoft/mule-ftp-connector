9824ea0e8cdd510397309b107569a95951f62282
Rodrigo Merino <elrodro83@users.noreply.github.com>
2017-09-08 19:24:33 -0300

    MULE-13363: Allow access to session properties from privileged API (#39)
    * Event interface split

--- src/test/java/org/mule/extension/ftp/FtpListTestCase.java
+++ src/test/java/org/mule/extension/ftp/FtpListTestCase.java
@@ -23,7 +23,7 @@ import org.mule.extension.file.common.api.exceptions.IllegalPathException;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.api.message.Message;
 import org.mule.runtime.api.streaming.bytes.CursorStreamProvider;
-import org.mule.runtime.core.api.InternalEvent;
+import org.mule.runtime.core.api.event.BaseEvent;
 import org.mule.runtime.core.api.processor.Processor;
 
 import java.util.ArrayList;
@@ -191,7 +191,7 @@ public class FtpListTestCase extends CommonFtpConnectorTestCase {
     }
 
     @Override
-    public InternalEvent process(InternalEvent event) throws MuleException {
+    public BaseEvent process(BaseEvent event) throws MuleException {
       Collection<Message> messageList = (Collection<Message>) event.getMessage().getPayload().getValue();
 
       for (Message message : messageList) {
