2eb249986e696c623bb9f67bf4951bf332f71b75
Alejandro García Marra <alemarra@gmail.com>
2018-07-12 12:49:50 -0300

    MULE-15301: FtpFileAttributes throw NPE when FTPFile date is null (#86)
    (cherry picked from commit 2929ff7)

--- src/main/java/org/mule/extension/ftp/internal/source/FtpDirectoryListener.java
+++ src/main/java/org/mule/extension/ftp/internal/source/FtpDirectoryListener.java
@@ -269,7 +269,13 @@ public class FtpDirectoryListener extends PollingSource<InputStream, FtpFileAttr
         item.setResult(result);
         item.setId(attributes.getPath());
         if (watermarkEnabled) {
-          item.setWatermark(attributes.getTimestamp());
+          if (attributes.getTimestamp() != null) {
+            item.setWatermark(attributes.getTimestamp());
+          } else {
+            LOGGER.warn(format("Use of watermark for files processing is enabled, but file [%s] does not have the"
+                + " corresponding modification timestamp. Watermark ignored for this file.",
+                               fullPath));
+          }
         }
       } catch (Throwable t) {
         LOGGER.error(format("Found file '%s' but found exception trying to dispatch it for processing. %s",
