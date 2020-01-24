c926e7613614b095496b0ce408d4638237c7040e
gabrieldalborgo <46535762+gabrieldalborgo@users.noreply.github.com>
2020-01-24 10:48:02 -0300

    MULE-17963: FTP COPY operation does not work when overwrite existing file (#154)

---
+++ src/test/java/org/mule/extension/ftp/FtpUtilsTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.ftp;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mule.extension.ftp.AllureConstants.FtpFeature.FTP_EXTENSION;
+import static org.mule.extension.ftp.internal.FtpUtils.createUrl;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import io.qameta.allure.Feature;
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.Test;
+
+@Feature(FTP_EXTENSION)
+public class FtpUtilsTestCase {
+
+  private final static String PROTOCOL = "ftp";
+  private final static String HOST = "127.0.0.1";
+  private final static int PORT = 80;
+  private final static String PATH = EMPTY;
+
+  private FTPClient client = mock(FTPClient.class);
+  private InetAddress address = mock(InetAddress.class);
+
+  @Test
+  public void validUrl() throws MalformedURLException {
+    when(address.getHostAddress()).thenReturn(HOST);
+    when(client.getRemoteAddress()).thenReturn(address);
+    when(client.getRemotePort()).thenReturn(PORT);
+    URL url = createUrl(client, null);
+    assertUrl(url);
+  }
+
+  @Test(expected = MalformedURLException.class)
+  public void invalidPort() throws MalformedURLException {
+    when(address.getHostAddress()).thenReturn(HOST);
+    when(client.getRemoteAddress()).thenReturn(address);
+    when(client.getRemotePort()).thenReturn(-2);
+    createUrl(client, null);
+  }
+
+  private void assertUrl(URL url) {
+    assertThat(url.getProtocol(), is(PROTOCOL));
+    assertThat(url.getHost(), is(HOST));
+    assertThat(url.getPort(), is(PORT));
+    assertThat(url.getPath(), is(EMPTY));
+  }
+}
