5734529963a20ea0c2ad067098c47dfbf7b8a649
rogerwang1102 <103075439+rogerwang1102@users.noreply.github.com>
2022-08-23 18:27:37 -0700

    W-11427407 update postActionGroup since the parent class in file common got updated (#297)
    * update postActionGroup since the parent class in file common has been updated and it is using latest file-common
    
    * update constructor
    
    * add unit test case
    
    * update import order

--- src/main/java/org/mule/extension/ftp/internal/source/PostActionGroup.java
+++ src/main/java/org/mule/extension/ftp/internal/source/PostActionGroup.java
@@ -52,14 +52,27 @@ public class PostActionGroup extends AbstractPostActionGroup {
   @Optional(defaultValue = "true")
   private boolean applyPostActionWhenFailed = true;
 
+  /**
+   * Enables you to overwrite the target file when the destination file has the same name
+   */
+  @Parameter
+  @Optional(defaultValue = "false")
+  private boolean overwrite = false;
+
 
   public PostActionGroup() {}
 
   public PostActionGroup(boolean autoDelete, String moveToDirectory, String renameTo, boolean applyPostActionWhenFailed) {
+    this(autoDelete, moveToDirectory, renameTo, applyPostActionWhenFailed, false);
+  }
+
+  public PostActionGroup(boolean autoDelete, String moveToDirectory, String renameTo, boolean applyPostActionWhenFailed,
+                         boolean overwrite) {
     this.autoDelete = autoDelete;
     this.moveToDirectory = moveToDirectory;
     this.renameTo = renameTo;
     this.applyPostActionWhenFailed = applyPostActionWhenFailed;
+    this.overwrite = overwrite;
   }
 
   public boolean isAutoDelete() {
@@ -76,5 +89,9 @@ public class PostActionGroup extends AbstractPostActionGroup {
 
   public boolean isApplyPostActionWhenFailed() {
     return applyPostActionWhenFailed;
+  }
+
+  public boolean getOverwrite() {
+    return overwrite;
   }
 }
