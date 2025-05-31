e2de732770ea78f9ca7c4a58674a8cd2ab03dad3
Kushagra Gupta <kushagragupta@salesforce.com>
2025-05-31 15:08:36 +0530

    coverage increased

--- src/test/java/org/mule/extension/ftp/internal/connection/FtpFileSystemTest.java
+++ src/test/java/org/mule/extension/ftp/internal/connection/FtpFileSystemTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2023 Salesforce, Inc. All rights reserved.
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.extension.ftp.internal.connection;
 
 import org.apache.commons.net.ftp.FTPClient;
@@ -11,43 +17,41 @@ import static org.mockito.Mockito.*;
 
 public class FtpFileSystemTest {
 
-    @Test
-    public void testIsFeatureSupported_logsErrorOnException() throws Exception {
-        FTPClient mockClient = mock(FTPClient.class);
-        when(mockClient.hasFeature(anyString())).thenThrow(new IOException("Test exception"));
+  @Test
+  public void testIsFeatureSupported_logsErrorOnException() throws Exception {
+    FTPClient mockClient = mock(FTPClient.class);
+    when(mockClient.hasFeature(anyString())).thenThrow(new IOException("Test exception"));
 
-        LockFactory mockLockFactory = mock(LockFactory.class);
+    LockFactory mockLockFactory = mock(LockFactory.class);
 
-        FtpFileSystem fileSystem = new FtpFileSystem(
-            mockClient,
-            "/",
-            mockLockFactory,
-            SingleFileListingMode.UNSET
-        );
+    FtpFileSystem fileSystem = new FtpFileSystem(
+                                                 mockClient,
+                                                 "/",
+                                                 mockLockFactory,
+                                                 SingleFileListingMode.UNSET);
 
-        boolean result = fileSystem.isFeatureSupported("SOME_FEATURE");
+    boolean result = fileSystem.isFeatureSupported("SOME_FEATURE");
 
-        assertFalse(result);
+    assertFalse(result);
+  }
+
+  @Test
+  public void testChangeToBaseDir_logsErrorOnIOException() throws Exception {
+    FTPClient mockClient = mock(FTPClient.class);
+    LockFactory mockLockFactory = mock(LockFactory.class);
+
+    when(mockClient.changeWorkingDirectory(anyString())).thenThrow(new IOException("Simulated IO error"));
+
+    String basePath = "/base/path";
+    FtpFileSystem fileSystem = new FtpFileSystem(
+                                                 mockClient,
+                                                 basePath,
+                                                 mockLockFactory,
+                                                 SingleFileListingMode.UNSET);
+
+    try {
+      fileSystem.changeToBaseDir();
+    } catch (RuntimeException e) {
     }
-
-    @Test
-    public void testChangeToBaseDir_logsErrorOnIOException() throws Exception {
-        FTPClient mockClient = mock(FTPClient.class);
-        LockFactory mockLockFactory = mock(LockFactory.class);
-
-        when(mockClient.changeWorkingDirectory(anyString())).thenThrow(new IOException("Simulated IO error"));
-
-        String basePath = "/base/path";
-        FtpFileSystem fileSystem = new FtpFileSystem(
-            mockClient,
-            basePath,
-            mockLockFactory,
-            SingleFileListingMode.UNSET
-        );
-
-        try {
-            fileSystem.changeToBaseDir();
-        } catch (RuntimeException e) {
-        }
-    }
+  }
 }
